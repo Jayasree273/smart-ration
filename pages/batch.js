0baa599ea2121d6e5ea9a817246e0e75d8934f75
Jayasree273 <jayasree200527@gmail.com>
2025-06-15 09:32:53 +0530

    Create batch.js

---
+++ pages/batch.js
@@ -0,0 +1,10 @@
+import BatchTrackerComponent from '../components/BatchTrackerComponent';
+
+export default function BatchPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1 style={{ textAlign: 'center', color: '#154734' }}>Batch Tracker</h1>
+      <BatchTrackerComponent />
+    </div>
+  );
+}
