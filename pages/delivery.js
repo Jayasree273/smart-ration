17c98931848b719565517e9f35efd7fa18f80533
Jayasree273 <jayasree200527@gmail.com>
2025-06-15 09:33:18 +0530

    Create delivery.js

---
+++ pages/delivery.js
@@ -0,0 +1,10 @@
+import DeliveryMonitorComponent from '../components/DeliveryMonitorComponent';
+
+export default function DeliveryPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1 style={{ textAlign: 'center', color: '#154734' }}>Delivery Monitor</h1>
+      <DeliveryMonitorComponent />
+    </div>
+  );
+}
